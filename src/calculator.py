d45843f291e34122cbf6089ed406a0f953c04b79
Prateek8dubey <prateek.dubey@datagrokr.com>
2020-06-18 15:27:14 +0530

    feat:added 2 new functionality of substractiona and divison

--- src/calculator.py
+++ src/calculator.py
@@ -1,4 +1,11 @@
 def calculator():
     print("this is calculator in python")
 
+def substraction():
+    print("this is function for substraction")
+def divison():
+    print("this is function for divison")
+
 calculator()
+substraction()
+divison()
